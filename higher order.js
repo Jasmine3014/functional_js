207014413f6da13edad9fbd452b11552c0babe6f
jasmine <jasminetripti5@gmail.com>
2021-08-01 15:48:12 +0600

    higher order -update

--- higher order.js
+++ higher order.js
@@ -1,4 +1,4 @@
-//higher-order function take a function as argument or can return afunction 
+//higher-order function take a function as argument or can return a function 
 arr=[5,8,0,3]
 arr.forEach(function(a){
     console.log(a)
