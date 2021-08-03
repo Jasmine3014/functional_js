0832c5039d4cb3a7a631b1a6ce502c9194a08583
jasmine <jasminetripti5@gmail.com>
2021-08-03 15:42:56 +0600

    fun-return-fun

--- fun-return-fun.js
+++ fun-return-fun.js
@@ -1,10 +1,20 @@
-function host(){
-    console.log("i am host function")
-     return function(){
-        console.log("i am child function")
-    }
-}
-var a = host();
-console.log(a())
+// function host(){
+//     console.log("i am host function")
+//      return function(){
+//         console.log("i am child function")
+//     }
+// }
+// var a = host();
+// console.log(a())
 
 //another way
+function host(){
+    console.log("i am host function")
+    function child(){
+        console.log("i am child function")
+    }
+return child;
+
+}
+var a = host();
+a();
