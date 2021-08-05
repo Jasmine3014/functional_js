de12ef76f54bbd2bf705df9250b3fdc52527a5ab
jasmine <jasminetripti5@gmail.com>
2021-08-05 16:33:14 +0600

    array-accesding

--- array.js
+++ array.js
@@ -1,6 +1,13 @@
+// arr=[87,90,4,67,23,65,68]
+// arr.sort(function(a,b){
+// return b-a;
+// }
+// );
+// console.log(arr);
+
 arr=[87,90,4,67,23,65,68]
 arr.sort(function(a,b){
-return b-a;
+return a-b;//accesending
 }
 );
 console.log(arr);
