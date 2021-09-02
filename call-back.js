461d96d6c5e26d88c95e3ddb16ba8dc7de6db2a5
jasmine <jasminetripti5@gmail.com>
2021-09-02 18:58:44 +0600

    call-back difference

--- call-back.js
+++ call-back.js
@@ -1,1 +1,3 @@
 //callback function works like function ,,,argument like another argument
+
+//ekta function e r ekta function call korte pari same vabei call back function kaj kore diiferen amra call back ta k argement akare variable er sateh arekta argument akare pass kori
